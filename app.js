d188f127b5a2a52401f961912b09ede20cb9d30a
montlein <montvlein@gmail.com>
2021-10-12 16:18:33 -0400

    [fix] production handler

--- app.js
+++ app.js
@@ -19,12 +19,3 @@ app.use(function(err, req, res, next) {
      res.status(err.status || 500).send(err);
      res.end();
 });
-
-// Handle production
-if (process.env.NODE_ENV === 'production') {
-    // Static folder
-    app.use(express.static(__dirname + '/public/'))
-
-    // Handle Single Page Aplication
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
-}
